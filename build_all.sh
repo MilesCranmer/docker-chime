3860c10cfe47278d1b16676c101d0f5b3c20191e
MilesCranmer <miles.cranmer@gmail.com>
2017-10-01 17:15:06 -0400

    Fix build of bonsai with numpy install

--- build_all.sh
+++ build_all.sh
@@ -1,4 +1,4 @@
 docker build -t chime-base -f Dockerfile .
 docker build -t chime-simd-helpers -f simd_helpers/Dockerfile .
 docker build -t chime-sp-hdf5 -f sp_hdf5/Dockerfile .
-#docker build -t chime-bonsai -f bonsai/Dockerfile .
+docker build -t chime-bonsai -f bonsai/Dockerfile .
